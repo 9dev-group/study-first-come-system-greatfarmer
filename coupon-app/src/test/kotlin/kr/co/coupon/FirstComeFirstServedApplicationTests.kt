49e2092e2087ea085edffddf2a7efa193e044a99
Seonghun Kang <lutis107@gmail.com>
2025-05-04 13:32:46 +0900

    add: 동시 쿠폰 발급 테스트

---
+++ coupon-app/src/test/kotlin/kr/co/coupon/FirstComeFirstServedApplicationTests.kt
@@ -0,0 +1,43 @@
+package kr.co.coupon
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class FirstComeFirstServedApplicationTests {
+	@Autowired
+	private lateinit var mockMvc: MockMvc
+
+	@Test
+	fun `동시 쿠폰 발급 테스트`() {
+		val numberOfUsers = 200
+		val latch = CountDownLatch(numberOfUsers)
+		val executor = Executors.newFixedThreadPool(4) // 병렬 요청 수
+
+		repeat(numberOfUsers) {
+			executor.submit {
+				try {
+					mockMvc.perform(
+						MockMvcRequestBuilders.post("/v1/coupon/issue")
+							.contentType(MediaType.APPLICATION_JSON)
+					).andExpect(MockMvcResultMatchers.status().isOk) // 또는 예상 응답 코드
+				} catch (e: Exception) {
+					println("요청 중 예외 발생: ${e.message}")
+				} finally {
+					latch.countDown()
+				}
+			}
+		}
+
+		latch.await() // 모든 요청이 끝날 때까지 대기
+	}
+}
